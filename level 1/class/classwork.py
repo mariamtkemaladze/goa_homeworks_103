7a81299e35ac52f08ef4f672589df36570891dee
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2026-04-21 21:21:03 +0400

    first commit

---
+++ level 1/class/classwork.py
@@ -0,0 +1,11 @@
+print ( 2 + 2)
+print ( 4 + 6 )
+
+print ( 3 - 1 )
+print ( 9 - 3 )
+
+print ( 5 * 7 )
+print ( 5 * 5 )
+
+print ( 21 / 3 )
+print ( 64 / 8 )
---
+++ level 1/home/homework.py
@@ -0,0 +1,47 @@
+print ( 5 + 5 )
+print ( 6 + 6 )
+print ( 4 + 23 )
+print ( 7 + 8 )
+print ( 10 + 56 )
+
+
+print ( 5 - 6 )
+print ( 10 - 56 )
+print ( 87 - 56 )
+print ( 198 - 23 )
+print (54 - 32 )
+
+print ( 45 * 65 )
+print ( 65 * 76 )
+print ( 67 * 69 )
+print ( 34 * 54 )
+print ( 32 * 65 )
+
+print ( 56 / 7 )
+print ( 45 / 9 )
+print ( 43 / 2 )
+print ( 67 / 8 )
+print ( 21 / 3 )
+
+print ( 54 // 8 )
+print ( 64 // 8 )
+print ( 43 // 87 )
+print ( 23 // 1 )
+print ( 43 // 2)
+
+print ( 5 % 4)
+print ( 3 % 6 )
+print ( 12 % 2 )
+print ( 12 % 12 )
+print ( 5 % 8 )
+
+print (3 ** 3 )
+print ( 12 ** 12 )
+print (45 ** 7 )
+print ( 89 ** 7 )
+print (45 ** 54 )
+
+#web development შედგება ორი ნაწილისგან: frontend and backend. ანუ დიზაინი და ინფო
+#string - ტექსტური ინფორმაცია
+#float - ათწილადები 
+#integer - numbers
