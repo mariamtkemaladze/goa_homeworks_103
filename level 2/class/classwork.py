977702d85d4a82aa3b12c5b32ad37327245f9350
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2026-04-22 21:16:39 +0400

    second commit

--- level 2/class/classwork.py
+++ level 2/class/classwork.py
@@ -1,2 +1,7 @@
-price = 34
-print(price)
+#price = 34
+#print(price)
+
+midterm = 50
+final = 30
+
+print(midterm + final)
---
+++ level 2/home/homework.py
@@ -0,0 +1,28 @@
+#variable is used for storing information in it.
+#we can reach a value by printing a valuable name.
+#we can't add string and int/float type of data together
+
+name = "mary"
+age = 16
+birthday = "8th march"
+country = "Georgia"
+city = "Tbilisi"
+fav_color = "black"
+fav_band = "xdiz"
+fav_subject = "math"
+fav_animal = "cat"
+bsf = "ele and dato"
+
+
+print(f"hi, my name is {name} and i am {age} years old. i was born on {birthday}, in {country},{city}. my fav color is {fav_color}. my fav band is {fav_band} and my fav subject is {fav_subject}. i love {fav_animal}s and my besties are {bsf}.")
+
+print(name + fav_color)
+print(fav_subject + fav_animal)
+
+
+x = 24
+y = 54
+z = 67
+
+print(x + y / z)
+print( z - y )
