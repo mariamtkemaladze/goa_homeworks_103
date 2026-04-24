98e86f1b44c60588b07a3d29470fcf526b2f4914
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2026-04-24 21:29:39 +0400

    second commit

---
+++ level 3/home/homework.py
@@ -0,0 +1,22 @@
+name = input("what's your name?/n")
+
+print(f"hello {name}!")
+
+
+x = int(input("enter x number: "))
+print(x * 2)
+
+
+y = int(input("enter y number: "))
+z = int(input("enter z number: "))
+
+print( y != z )
+print( y >= z )
+print( y <= z )
+print( z == y )
+print( y == y )
+
+
+#input შემოაქვს ინფორმაცია პროგრამაში 
+#int() ფუნქცია გამოიყენება შემოტანილი ინფორმაციის ინტეჯერად გადაქცევისთვის
+#str() ფუნქცია ინფორმაციას აქცევს სტრინგად
